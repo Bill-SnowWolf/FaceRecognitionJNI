411a76b5bd63b19be4c54449d2602be44133eb26
Bill Zhou <bill@micrometrics.ca>
2015-03-31 17:05:46 -0400

    Initial Commit

---
+++ TestJNI/TestJNI.h
@@ -0,0 +1,13 @@
+//
+//  TestJNI.h
+//  TestJNI
+//
+//  Created by Xingnan Zhou on 2015-03-31.
+//  Copyright (c) 2015 SnowWolf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestJNI : NSObject
+
+@end
---
+++ TestJNI/TestJNI.m
@@ -0,0 +1,13 @@
+//
+//  TestJNI.m
+//  TestJNI
+//
+//  Created by Xingnan Zhou on 2015-03-31.
+//  Copyright (c) 2015 SnowWolf. All rights reserved.
+//
+
+#import "TestJNI.h"
+
+@implementation TestJNI
+
+@end
